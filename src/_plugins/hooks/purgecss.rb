8ee0e2ae8b3a23d9c756f462211fc4695a23be2c
PAUL STROOT <paulstroot@gmail.com>
2022-11-23 14:05:18 -0600

    Clean up some unused scripts

--- src/_plugins/hooks/purgecss.rb
+++
@@ -1,4 +0,0 @@
-Jekyll::Hooks.register(:site, :post_write) do |_site|
-  # Run purgecss command.
-  system ("purgecss --config ./purgecss.config.js --output #{_site.config.fetch("destination")}/css")
-end
